704757b780598a9811b69d80c6c9e34f7d0413fc
19223229 <MPDyakovskaya@sberbank.ru>
2021-06-04 13:32:45 +0300

    DB init now reads data from file

--- src/main/java/ru/sberbank/bankapi/DataAccessObject/DBConnector.java
+++ src/main/java/ru/sberbank/bankapi/DataAccessObject/DBConnector.java
@@ -1,6 +1,7 @@
 package ru.sberbank.bankapi.DataAccessObject;
 
-import java.io.BufferedReader;
+import java.io.*;
+import java.math.BigDecimal;
 import java.sql.*;
 
 /**
@@ -15,6 +16,12 @@ public class DBConnector {
 
     public static Connection con;
     public static ResultSet rs;
+    /**
+     * Пути к файлам с ресурсами для заполнения БД
+     */
+    private static final String CLIENTS_RESOURCE_FILE = "src/main/resources/DBContents/clients.txt";
+    private static final String ACCOUNTS_RESOURCE_FILE = "src/main/resources/DBContents/accounts.txt";
+    private static final String CARDS_RESOURCE_FILE = "src/main/resources/DBContents/cards.txt";
 
     /**
      * выполняется подключение к БД, хранящейся в памяти
@@ -102,34 +109,53 @@ public class DBConnector {
     }
 
     /**
-     * заполняет БД данными
+     * заполняет БД данными из файлов
+     * @see DBConnector приватные переменнные
      * @return 1 в случае успешного заполнения БД, 0 в случае ошибки
      */
     public static int dbInit() {
         try {
-            Statement statement = con.createStatement();
-            statement.executeUpdate("INSERT INTO CLIENT (LOGIN) VALUES ( 'RITA'); " +
-                    "INSERT INTO CLIENT (LOGIN) VALUES ( 'JOHN');" +
-                    "INSERT INTO CLIENT (LOGIN) VALUES ( 'VASYA')");
-
-            statement.executeUpdate("INSERT INTO ACCOUNT (NUMBER, BALANCE, CLIENT_ID) VALUES ( '8888', 120.5, 1 ); " +
-                    "INSERT INTO ACCOUNT (NUMBER, BALANCE, CLIENT_ID) VALUES ('9999', 340.6, 2)");
-
-            statement.executeUpdate("INSERT INTO CARD(NUMBER, ACCOUNT_ID) VALUES ( '0000000000000000', 1 );" +
-                    "INSERT INTO CARD(NUMBER, ACCOUNT_ID) VALUES ( '0000000000000001', 1 ); " +
-                    "INSERT INTO CARD(NUMBER, ACCOUNT_ID) VALUES ( '0000000000000002', 2 ); " +
-                    "INSERT INTO CARD(NUMBER, ACCOUNT_ID) VALUES ( '0000000000000003', 2 )");
-
+            clientsInit();
+            accountsInit();
+            cardsInit();
             return 1;
-        } catch (SQLException throwables) {
+        } catch (SQLException | IOException throwables) {
             throwables.printStackTrace();
             return 0;
         }
     }
 
-    public static int dbInit(int intValue) {
-        BufferedReader reader;
-        return 0;
+    private static void clientsInit() throws IOException, SQLException {
+        BufferedReader reader = new BufferedReader(new FileReader(DBConnector.CLIENTS_RESOURCE_FILE));
+        PreparedStatement statement = con.prepareStatement("INSERT INTO CLIENT (LOGIN) VALUES ( ? ); ");
+        while (reader.ready()) {
+            String login = reader.readLine();
+            statement.setString(1, login);
+            statement.executeUpdate();
+        }
+    }
+
+    private static void accountsInit() throws IOException, SQLException {
+        BufferedReader reader = new BufferedReader(new FileReader(DBConnector.ACCOUNTS_RESOURCE_FILE));
+        PreparedStatement statement = con.prepareStatement("INSERT INTO ACCOUNT (NUMBER, BALANCE, CLIENT_ID) VALUES (?, ?, ?);");
+        while (reader.ready()) {
+            String[] accountParams = reader.readLine().split(";");
+            statement.setString(1, accountParams[0]);
+            statement.setBigDecimal(2, new BigDecimal(accountParams[1]));
+            statement.setLong(3, Long.parseLong(accountParams[2]));
+            statement.executeUpdate();
+        }
+    }
+
+    private static void cardsInit() throws IOException, SQLException {
+        BufferedReader reader = new BufferedReader(new FileReader(DBConnector.CARDS_RESOURCE_FILE));
+        PreparedStatement statement = con.prepareStatement("INSERT INTO CARD(NUMBER, ACCOUNT_ID) VALUES ( ?, ? );");
+        while (reader.ready()) {
+            String[] accountParams = reader.readLine().split(";");
+            statement.setString(1, accountParams[0]);
+            statement.setLong(2, Long.parseLong(accountParams[1]));
+            statement.executeUpdate();
+        }
     }
 
 
